464d14aec313a0f4cc0a5650ed1f8de719c21eb5
rifda <rifda.tasyrikah@gmail.com>
2023-05-26 17:06:18 +0700

    final

--- ppam-museek/screens/schedule/index.js
+++ ppam-museek/screens/schedule/index.js
@@ -1,118 +1,5 @@
-// import { FlatList, StyleSheet, View, Alert } from "react-native";
-// import { ActivityIndicator, Appbar, Button, FAB, IconButton, List, Text } from "react-native-paper";
-// import theme from "../../config/theme";
-
-// import auth from '@react-native-firebase/auth';
-// import { useNavigation, useRoute } from "@react-navigation/native";
-// import { SafeAreaView } from "react-native-safe-area-context";
-
-// import firestore from '@react-native-firebase/firestore';
-// import { useEffect, useState } from "react";
-// import { useAuth } from "../../contexts/AuthProvider";
-// import { useCollectionData } from 'react-firebase-hooks/firestore';
-
-// export default function Schedule() {
-//     const navigation = useNavigation();
-//     const route = useRoute();
-//     const { user } = useAuth();
-//     const query = firestore()
-//         .collection("schedule")
-//         .where("userId", "==", user.uid)
-//         .orderBy("createdAt", "desc");
-
-
-//     const [data, loading] = useCollectionData(query, { idField: "id" });
-
-//     // data = [
-//     //     {
-//     //         date: "kapan",
-//     //         location: "dimana",
-//     //     },
-//     //     {
-//     //         date: "when",
-//     //         location: "where"
-//     //     }
-//     // ]
-    
-    
-//         // auth().signOut();
-//     console.log("ini di page shcedule")
-//     console.log(user.uid);
-    
-//     console.log(data);
-//     // console.log(data[0].name);
-
-
-//     return <SafeAreaView style={styles.container}>
-//         <Appbar>
-//             <Appbar.Content title=" Jadwal Belajarmu" />
-//         </Appbar>
-
-//         {loading ?
-//             <View style={styles.loading}>
-//                 <ActivityIndicator />
-//             </View>
-//             :
-//             <FlatList
-
-//                 data={data}
-//                 renderItem={({ item }) => {
-//                     const { date, location } = item;
-//                     return <List.Item
-//                         // left={props => <List.Icon {...props} icon="checkbox-blank-circle" />}
-//                         // file-eye-outline
-//                         style={styles.list}
-//                         title={date}
-//                         description={location}
-//                         right={props => <View {...props}>
-//                             <View style={styles.actionBtns}>
-//                                 <IconButton mode="outlined" onPress={() => navigation.navigate("Details", {item})} icon="arrow-right-bold" />                                
-//                             </View>
-//                         </View>}
-//                     />
-//                 }}
-//             />}
-
-//     </SafeAreaView>
-// }
-
-// const styles = StyleSheet.create({
-//     container: {
-//         flex: 1
-//     },
-//     buttonContainer: {
-//         marginTop: 20
-//     },
-//     title: {
-//         color: theme.colors.primary
-//     },
-//     fab: {
-//         position: "absolute",
-//         right: 40,
-//         bottom: 40
-//     },
-//     actionBtns: {
-//         flexDirection: "row"
-//     },
-//     loading: {
-//         flex: 1,
-//         justifyContent: "center",
-//         alignItems: "center"
-//     },
-//     list: {
-//         padding: 5,
-//         backgroundColor:"white",
-//         // marginTop:10,
-//         marginHorizontal:10,
-//         marginBottom: 10,
-//         borderRadius: 10
-//     }
-// })
-
-
-
 import { FlatList, StyleSheet, View, Alert } from "react-native";
-import { ActivityIndicator, Appbar, Button, FAB, IconButton, List, Text } from "react-native-paper";
+import { ActivityIndicator, Appbar, Button, FAB, IconButton, List, Text, Avatar } from "react-native-paper";
 import theme from "../../config/theme";
 
 import auth from '@react-native-firebase/auth';
@@ -120,53 +7,25 @@ import { useNavigation, useRoute } from "@react-navigation/native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import firestore from '@react-native-firebase/firestore';
+import storage from '@react-native-firebase/storage';
 import { useEffect, useState } from "react";
 import { useAuth } from "../../contexts/AuthProvider";
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 
-export default function Home() {
+
+export default function Schedule() {
     const navigation = useNavigation();
     const route = useRoute();
     const { user } = useAuth();
-
-    // console.log(user.uid);
     const query = firestore()
         .collection("schedule")
-        .where("userId", "==", user.uid)
-        // .orderBy("createdAt", "desc");
-
-        // const query = firestore()
-//         .collection("schedule")
-//         .where("userId", "==", user?.uid)
-//         .orderBy("createdAt", "desc");
+        .where("userId", "==", user.uid);
+    // .where("instrument", "==", route.params.category);
 
         // auth().signOut();
     const [data, loading] = useCollectionData(query, { idField: "id" });
 // 
-    // console.log(data);
-    // console.log(user.uid, user.displayName)
-
-    // for( var i = 0; i < data.length; i++){ 
-    
-    //     if ( data[i].userId === 5) { 
-    
-    //         arr.splice(i, 1); 
-    //     }
-    
-    // }
-
-    // data = [
-    //     {description:
-    //         "Pemain piano berpengalaman lebih dari 10 tahun"
-    //         ,fee:
-    //         "IDR 120.000",
-    //         instrument:
-    //         "Piano",
-    //         names:
-    //         "James Adam"}
-
-    // ]
-    if (data.length != 0) {
+    // if (data.length != 0) {
         return <SafeAreaView style={styles.container}>
             <Appbar>
                 <Appbar.Content title="Jadwal Belajar Kamu" style={{alignItems: "center"}} />
@@ -218,41 +77,36 @@ export default function Home() {
             {/* <View style={{padding:20}}></View> */}
 
         </SafeAreaView>
-    } else {
-        return <SafeAreaView style={styles.container}>
-        <Appbar>
-            <Appbar.Content title="Jadwal Belajar Kamu" style={{alignItems: "center"}} />
-        </Appbar>
+    // } else {
+    //     return <SafeAreaView style={styles.container}>
+    //     <Appbar>
+    //         <Appbar.Content title="Jadwal Belajar Kamu" style={{alignItems: "center"}} />
+    //     </Appbar>
 
-        {loading ?
-                <View style={styles.loading}>
-                    <ActivityIndicator />
-                </View>
-                :
+    //     {loading ?
+    //             <View style={styles.loading}>
+    //                 <ActivityIndicator />
+    //             </View>
+    //             :
                 
-                <View style={styles.titleContainer}>
-                    <Text variant="headlineSmall" style={styles.title}>Belum ada Jadwal Belajar Musik dengan Guru Privat Pilihanmu Nih</Text>
-                </View>
-        }
+    //             <View style={styles.titleContainer}>
+    //                 <Text variant="headlineSmall" style={styles.title}>AAA MAU NANGIS </Text>
+    //             </View>
+    //     }
         
-        </SafeAreaView>
-    }
+    //     </SafeAreaView>
+    // }
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1
     },
-    buttonContainer: {
-        marginTop: 20,
-        width:" 90%"
-    },
     titleContainer: {
-        flex:1,
-        width: "90%",
+        flex: 1,    
         justifyContent: "center",
-        alignSelf: "center",
-        // backgroundColor:"white",
+        alignItems: "center",
+        marginTop: 20
     },
     title: {
         color: theme.colors.primary,
